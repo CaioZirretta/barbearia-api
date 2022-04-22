29eb1ab3c66939c651965eb254e3d3f6c2297567
Caio Zirretta <84157226+CaioZirretta@users.noreply.github.com>
2022-04-22 09:58:42 -0300

    1.6

--- Scripts/Script.sql
+++
@@ -1,7 +0,0 @@
-select * from prestadores p; 
-
-select * from clientes c; 
-
-delete from prestadores;
-
-delete from clientes;
--- src/main/java/com/barbearia/application/BarbeariaApiApplication.java
+++ src/main/java/com/barbearia/application/BarbeariaApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BarbeariaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarbeariaApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(BarbeariaApiApplication.class, args);
+    }
 
 }
